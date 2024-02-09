17f165c8aae1bc0773a2ff9718983e51a0c254b1
LorisDXB <loris.gode@epitech.eu>
2024-02-09 01:28:37 +0000

    updated startenv.sh

--- startenv.sh
+++ startenv.sh
@@ -1,5 +1,5 @@
 python3 -m venv ~/.venvs/discord
 source ~/.venvs/discord/bin/activate
 pip freeze > requirements.txt
-export DISCORD_TOKEN=MTIwNDc5NTE0NDg4OTY5NjI5Ng.GwOci2.U0VGVwPrrEsb1Cey4DRYxkwYEs-ViFtTEjDEno
+export DISCORD_TOKEN="TOKEN HERE"
 export DISCORD_GUILD=epilift
