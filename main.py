f3ddb03b892de74e6e2ed808f82f2175e1f54c34
LorisDXB <loris.gode@epitech.eu>
2024-02-23 17:29:17 +0000

    added lookup func

--- main.py
+++ main.py
@@ -72,9 +72,11 @@ async def on_message(message):
                 result = cursor.fetchone()
                 if result:
                     cursor.execute("UPDATE sbd SET squat = ?, bench = ?, deadlift = ?, sbd = ? WHERE name = ?", (parts[3], parts[4], parts[5], (int(parts[3]) + int(parts[4]) + int(parts[5])) / 3, parts[2]))
+                    cursor.execute("UPDATE lookup SET squatvid = ?, benchvid = ?, deadliftvid = ? WHERE name = ?", (parts[6], parts[7], parts[8], parts[2]))
                     await message.channel.send(f"L'utilisateur {parts[2]} a été mis à jour avec succès !")
                 else:
-                    cursor.execute("INSERT INTO sbd (name, squat, bench, deadlift) VALUES (?, ?, ?, ?)", (parts[2], parts[3], parts[4], parts[5]))
+                    cursor.execute("INSERT INTO sbd (name, squat, bench, deadlift) VALUES (?, ?, ?, ?)", (str(parts[2]), parts[3], parts[4], parts[5]))
+                    cursor.execute("INSERT INTO lookup (name, squatvid, benchvid, deadliftvid) VALUES (?, ?, ?, ?)", (str(parts[2]), parts[6], parts[7], parts[8]))
                     await message.channel.send(f"L'utilisateur {parts[2]} a été ajouté au classement avec succès !")
                 conn.commit()
             except (IndexError, ValueError):
@@ -97,6 +99,28 @@ async def on_message(message):
                 await message.channel.send("Pas assez d'arguments, 4 arguments nécessaire")
         else:
             await message.channel.send("Vous n'avez pas les permissions.")
-    ####  ####################
+    ####  LOOKUP FUNCTION ####################
+    if parts[0] == "!lookup":
+        print("lookup!");
+        try:
+            cursor.execute("SELECT lookup.name, lookup.squatvid, lookup.benchvid, lookup.deadliftvid, sbd.squat, sbd.bench, sbd.deadlift FROM lookup JOIN sbd ON lookup.name = sbd.name WHERE lookup.name = ?", (parts[1],))
+            results = cursor.fetchone()
+            if results:
+                name = results[0]
+                squatvid = results[1]
+                benchvid = results[2]
+                deadliftvid = results[3]
+                squat = results[4]
+                bench = results[5]
+                deadlift = results[6]
+                embed = discord.Embed(title="Lookup", description="Voici les statistiques de cette personne:", color=0xffd700)
+                value = f"Prenom: {name},\n Squat: {squatvid} {squat}kg,\n Bench: {benchvid} {bench}kg,\n Deadlift: {deadliftvid} {deadlift}kg"
+                embed.add_field(name=f"Utilisateur:", value=value, inline=False)
+                embed.set_footer(text="Merci de DM lorisdxb si vous trouvez un bug! :)")
+                await message.channel.send(embed=embed)
+            else:
+                await message.channel.send("Aucun résultat trouvé.")
+        except (IndexError, ValueError):
+            await message.channel.send("Erreur lors de l'exécution de la requête.")
 
 client.run(token)
--- startenv.sh
+++ startenv.sh
@@ -1,5 +1,5 @@
 python3 -m venv ~/.venvs/discord
 source ~/.venvs/discord/bin/activate
 pip freeze > requirements.txt
-export DISCORD_TOKEN="TOKEN HERE"
+export DISCORD_TOKEN="MTIwNDc5NTE0NDg4OTY5NjI5Ng.G3Dpjc.BeTqb6xAbzOQKgC1oh-Qw1W563ruIXJU96b8wo"
 export DISCORD_GUILD=epilift
